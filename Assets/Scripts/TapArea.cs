01ce8e11439f4ca2dc64fb1716ac38331936a775
Rizal <dickyarisya@yahoo.com>
2021-04-13 14:17:46 +0700

    Task 2

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour,IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
